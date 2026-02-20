07f8f1c971cf26f4c3964219e304111c7f72529f
Emory Adelman <168493902+emorya@users.noreply.github.com>
2026-02-20 14:34:36 -0800

    Refactor touch_tip call in pre_DNA_test.py
    Updated touch_tip method to use default parameters.

--- pre_DNA_test.py
+++ pre_DNA_test.py
@@ -265,7 +265,7 @@ def run(protocol: protocol_api.ProtocolContext):
          p20.dispense(1, dest_tube.bottom(2))
          # p20.blow_out(dest_tube.top(-1.5))
          # p20.blow_out(dest_tube.top(-1)) 
-         p20.touch_tip(dest_tube.top(-0.5))
+         p20.touch_tip()
          p20.blow_out(dest_tube.top()) 
          p20.drop_tip()
 
