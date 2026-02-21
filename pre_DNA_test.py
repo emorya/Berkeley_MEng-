7f7e16e67f5c80a229fd98df958bb07ad5cb3e72
Emory Adelman <168493902+emorya@users.noreply.github.com>
2026-02-20 16:01:43 -0800

    Change starting tip from G1 to A7

--- pre_DNA_test.py
+++ pre_DNA_test.py
@@ -199,7 +199,7 @@ def run(protocol: protocol_api.ProtocolContext):
     vol_water = 2.24
 
     #THIS SHIT BETTER WORK 
-    p20.starting_tip = tiprack_20a['G1']
+    p20.starting_tip = tiprack_20a['A7']
 
     def add_to_mix(vol_uL, src, label):
         protocol.comment(f"Add {label}: {vol_uL} uL")
